d83122982865f6fc18ebf4458ed8d2bd73295328
Gamika Jayawardana <gamikakj@gmail.com>
2025-10-04 20:44:54 +0530

    Update branding, assets, and dependencies
    Rebranded site metadata, removed custom CSS variables, and updated favicon. Added new image and SVG assets for about, hero, services, testimonials, and work sections. Removed default SVG files from public directory. Updated Tailwind config and added new dependencies: framer-motion, react-countup, react-icons, and swiper.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,56 +7,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
+    container: {
+      center:true,
+      padding: "15px"
+    },
+
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px"
+    },
+
   	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+      colors: {
+        primary: "#FF6363",
+        secondary: "#3A86FF",
+        accent: "#8338EC",
+        border: "#E0E0E0",
+      },
+      boxShadow: {
+        custom: "0px 4px 54px 10px rgba(18, 19, 21, 0.06)",
+      },
+  	},
   },
   plugins: [require("tailwindcss-animate")],
 }
-
