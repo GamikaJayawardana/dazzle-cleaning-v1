6c598a55e8f1e3e53711321343a0ff58dc5e0880
Gamika Jayawardana <gamikakj@gmail.com>
2025-10-05 23:35:12 +0530

    Add responsive logo styling and update Logo component
    Introduced logo.css for responsive logo image sizing and updated Logo.jsx to use react-scroll's 'to' prop and apply the new CSS class. This improves logo appearance across devices and ensures smooth scrolling navigation.

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-scroll'
+import './Logo.css' // Import your CSS file
 
 const Logo = () => {
   return (
     <div>
-        <Link href="">
-          <img src="/assets/logo.png" alt="Logo" width={150} />    
-        </Link>
+      <Link to="home" smooth={true} duration={500}> {/* 'to' prop is necessary for react-scroll */}
+        <img src="/assets/logo.png" alt="Logo" className="logo-img" />    
+      </Link>
     </div>
   )
 }
