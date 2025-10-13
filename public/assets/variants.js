b1b96cb98ac0d4debf5f11e705275f050a4719b7
Gamika Jayawardana <gamikakj@gmail.com>
2025-10-13 21:11:57 +0530

    Reduce fadeIn animation duration to 0.5s
    The duration of the fadeIn animation was changed from 1 second to 0.5 seconds to make transitions faster and improve UI responsiveness.

--- public/assets/variants.js
+++ public/assets/variants.js
@@ -12,7 +12,7 @@ export const fadeIn = (direction, delay) => {
       opacity: 1,
       transition: {
         type: "tween",
-        duration: 1,
+        duration: 0.5,
         delay: delay,
         ease: [0.25, 0.25, 0.25, 0.75],
       },
